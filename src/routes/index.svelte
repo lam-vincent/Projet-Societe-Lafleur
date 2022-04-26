2b8a78d99327359759fe6b384e91bdd5f5926f0f
Vincent <mrccniv@gmail.com>
2022-04-26 19:48:18 +0200

    block out layout

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,47 @@
-<h1>hello world</h1>
+<script>
+	import '../app.css';
+</script>
+
+<div class="wrapper">
+	<div class="header">
+		<span>header</span>
+	</div>
+	<div class="middle">
+		<div class="menu"><span>menu</span></div>
+		<div class="page"><span>page</span></div>
+	</div>
+	<div class="footer"><span>footer</span></div>
+</div>
+
+<style>
+	.wrapper {
+		display: flex;
+		height: 100vh;
+		flex-direction: column;
+	}
+
+	.header {
+		height: 8rem;
+		background-color: blue;
+	}
+
+	.middle {
+		display: flex;
+		flex-grow: 1;
+	}
+
+	.menu {
+		width: 20rem;
+		background-color: green;
+	}
+
+	.page {
+		background-color: red;
+		flex-grow: 1;
+	}
+
+	.footer {
+		height: 8rem;
+		background-color: yellow;
+	}
+</style>
