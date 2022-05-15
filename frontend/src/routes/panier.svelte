003d972a9726da5324b2f1725fad61519eb7534b
Vincent <mrccniv@gmail.com>
2022-05-15 10:29:28 +0200

    sign-in

---
+++ frontend/src/routes/panier.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Layout from '../components/Layout.svelte';
+    import Text from '../components/Text.svelte';
+</script>
+
+<Layout>
+	<Text label="Nom" value="Entrez votre nom" />
+	<Text label="Prénom" value="Entrez votre prénom" />
+</Layout>
