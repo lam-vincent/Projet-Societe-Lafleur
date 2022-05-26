26d34a5dce9649617c9ef79426d7a7665cc666b7
Vincent <mrccniv@gmail.com>
2022-05-26 23:04:32 +0200

    panier uncomplete

---
+++ frontend/src/components/Item.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import FaTrashAlt from 'svelte-icons/fa/FaTrashAlt.svelte';
+</script>
+
+<div class="card">
+	<div class="item">
+		<span class="name">name</span>
+		<span>price</span>
+		<span>amount</span>
+		<FaTrashAlt />
+	</div>
+</div>
+
+<style>
+	.card {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		background-color: lightgreen;
+	}
+</style>
--- frontend/src/routes/panier.svelte
+++ frontend/src/routes/panier.svelte
@@ -1,8 +1,62 @@
 <script>
 	import Layout from '../components/Layout.svelte';
-    import List from '../components/List.svelte';
+	import FaTrashAlt from 'svelte-icons/fa/FaTrashAlt.svelte';
 </script>
 
 <Layout>
-	<List/>
+	<div class="wrapper">
+		<div class="container">
+			<div class="card">
+				<div class="info">
+					<h1 class="title">name</h1>
+					<span class="price">price</span>
+					<span class="amount">amount</span>
+				</div>
+				<button class="logo"><FaTrashAlt /></button>
+			</div>
+		</div>
+	</div>
 </Layout>
+
+<style>
+	.wrapper {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.container {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.card {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		background-color: lightgreen;
+		width: 50rem;
+		height: 5rem;
+		border-radius: 4rem;
+	}
+
+	.info {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		margin-left: 2rem;
+		width: 35rem;
+	}
+
+	.title {
+		font-size: 1.5rem;
+	}
+
+	.logo {
+		width: 2rem;
+		background-color: lightgreen;
+		border-radius: 8rem;
+		margin-right: 2rem;
+	}
+</style>
