93e0b96f25162c80d28689296d170cf4c82c9381
villirion <75848800+villirion@users.noreply.github.com>
2022-05-27 14:33:10 +0200

    Update contact.svelte

--- frontend/src/routes/contact.svelte
+++ frontend/src/routes/contact.svelte
@@ -40,7 +40,10 @@
 	    .build()
 
 	  return this.aws.execute(request)
-	}	
+	}
+	function submit(){
+		document.getElementsByTagName
+	}
 </script>
 
 <Layout>
@@ -49,11 +52,6 @@
 	<Text label="Nom" value="Entrez votre nom" />
 	<Text label="Prénom" value="Entrez votre prénom" />
 	<Email label="Email" value="monmail@monsite.org" />
-	<!-- 
-	<Radio label="Genre" />
-	<Date label="Date de naissance" />
-	<Select label="Fonction" fruits={['enseignant', 'enseignant']} />
-	 -->
 	<Text label="Sujet" value="Entrez le sujet de votre mail" />
 	<Textarea label="Contenu" helper="Tapez ici votre mail" />
 
